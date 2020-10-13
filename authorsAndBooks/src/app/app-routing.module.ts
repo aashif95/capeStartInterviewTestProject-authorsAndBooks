3c0715dcd343044285c2d3af9cfd8ec9224b5a16
aashif95 <aashifrahman@gmail.com>
2020-10-13 21:36:55 +0530

    UI base code added

---
+++ authorsAndBooks/src/app/app-routing.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { BooksComponent } from './books/books.component';
+import { AuthorsComponent } from './authors/authors.component';
+
+const routes: Routes = [
+  {
+    path: 'books',
+    component: BooksComponent
+  },
+  {
+    path: 'authors',
+    component: AuthorsComponent
+  },
+  { path: '',   redirectTo: '/books', pathMatch: 'full' },
+  // { path: '**', component:  AuthorsComponent},
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ authorsAndBooks/src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { GraphQLModule } from './graphql.module';
+import { HttpClientModule } from '@angular/common/http';
+import { MaterialModule } from './mat.module';
+import { BooksComponent } from './books/books.component';
+import { AuthorsComponent } from './authors/authors.component';
+@NgModule({
+  declarations: [
+    AppComponent,
+    BooksComponent,
+    AuthorsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    GraphQLModule,
+    HttpClientModule,
+    MaterialModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
